dfce0417215c65db78c8a794d8ec9ee6b441f755
Steve Davis <deevodavis71@gmail.com>
2017-07-22 08:33:27 +0100

    Test commit 1

--- src/main/java/com/sjd/springcontext/SpringcontextApplication.java
+++ src/main/java/com/sjd/springcontext/SpringcontextApplication.java
@@ -1,18 +1,18 @@
 package com.sjd.springcontext;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan ("com.sjd")
-@EnableAutoConfiguration
+//@Configuration
+@ComponentScan("com.sjd")
+// @EnableAutoConfiguration
 @SpringBootApplication
-public class SpringcontextApplication {
+public class SpringcontextApplication
+{
 
-	public static void main(String[] args) {
+	public static void main (String[] args)
+	{
 		SpringApplication.run(SpringcontextApplication.class, args);
 	}
 }
